91540ac8c0169c0013b98778251398700bd40084
Jack Darlington <jackdarlington2001@gmail.com>
2023-08-30 16:51:35 +1200

    Update ItemPickup.cs
    Item now follows player once in a specified distance from player

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
+    private GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        
+        if (distance < 10.0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, 1.0f * Time.deltaTime);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        print("Collision detected!");
-
-        float distance = Vector3.Distance(transform.position, other.transform.position);
-        float step = 10.0f * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, other.transform.position, step);
-        if (distance < 0.1f)
-        {
-            // Destroy
-        }
         IInventory inventory = other.GetComponent<IInventory>();
 
         if (inventory != null)
