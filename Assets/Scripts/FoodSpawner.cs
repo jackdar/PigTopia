0015aa86103977b918a4c9221188494f87a0221f
Anarchry <83636074+Anarchry@users.noreply.github.com>
2023-08-31 12:30:29 +1200

    a
    a

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -9,7 +9,7 @@ public class FoodSpawner : MonoBehaviour
 
     private void Awake()
     {
-        if(ins == null)
+        if (ins == null)
         {
             ins = this;
         }
@@ -17,10 +17,9 @@ public class FoodSpawner : MonoBehaviour
     #endregion
 
     public GameObject Food;
-    public List<GameObject> Player = new List<GameObject>();
     public List<GameObject> Create_Food = new List<GameObject>();
     public int Max_Food = 50;
-    public float Time_To_Instantiate = 0.5f;
+    public float Time_To_Instantiate = 0.0f;
     public Vector2 pos;
 
     // Start is called before the first frame update
@@ -31,48 +30,35 @@ public class FoodSpawner : MonoBehaviour
 
     public IEnumerator CreateFood()
     {
-        yield return new WaitForSecondsRealtime(Time_To_Instantiate);
-        if (Create_Food.Count <= Max_Food)
+        while (true)
         {
-            Vector2 Position = new Vector2(Random.Range(-pos.x, pos.x), Random.Range(-pos.y, pos.y));
-            Position /= 2;
-            GameObject m =  Instantiate(Food, Position, Quaternion.identity);
-            AddFood(m);
+            if (Create_Food.Count <= Max_Food)
+            {
+                Vector2 Position = new Vector2(Random.Range(-pos.x, pos.x), Random.Range(-pos.y, pos.y));
+                Position /= 2;
+                GameObject m = Instantiate(Food, Position, Quaternion.identity);
+                AddFood(m);
+            }
+            yield return new WaitForSeconds(Time_To_Instantiate);
         }
-        StartCoroutine(CreateFood());
     }
 
     public void AddFood(GameObject m)
     {
-        if(Create_Food.Contains(m) == false)
+        if (Create_Food.Contains(m) == false)
         {
             Create_Food.Add(m);
-            for (int i = 0; i < Player.Count; i++)
-            {
-                Eat e = Player[i].GetComponent<Eat>();
-                if (e != null)
-                {
-                    e.AddFood(m);
-                }
-            }
         }
     }
 
     public void RemoveFood(GameObject m)
     {
-        if(Create_Food.Contains(m) == true)
+        if (Create_Food.Contains(m) == true)
         {
             Create_Food.Remove(m);
-            for (int i = 0; i < Player.Count; i++)
-            {
-                Eat e = Player[i].GetComponent<Eat>();
-                if (e != null)
-                {
-                    e.RemoveFood(m);
-                }
-            }
         }
     }
+
     public void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
