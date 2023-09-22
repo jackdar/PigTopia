9931dfefd5192894a7b4a5051a20be4b14b3288f
jackdar <jackdarlington2001@gmail.com>
2023-09-22 16:24:16 +1200

    Pig scale increase, camera zoom out, and windows build
    - Pig now increases scale when eating food
    - Camera zooms out every 20 food consumed
    - Game build can be found under Build folder

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,28 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Pickup : MonoBehaviour
 {
     private GameObject Player;
     private FoodSpawner foodSpawner;
-    public float grow = 0.05f;
-    public float pickupRange;
-    public float flightSpeed;
+    private CinemachineVirtualCamera vcam;
+    private AudioSource popSound;
+
+    [SerializeField] private float pickupRange;
+    [SerializeField] private float flightSpeed;
+
+    [SerializeField] private float playerGrowSpeed;
+    [SerializeField] private float playerGrowAmount;
+
+    [SerializeField] private float cameraGrowSpeed;
+    [SerializeField] private float cameraGrowAmount;
 
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         foodSpawner = FoodSpawner.ins;
+        vcam = GameObject.Find("Player Camera").GetComponent<CinemachineVirtualCamera>();
+        popSound = gameObject.GetComponent<AudioSource>();
     }
 
     void Update()
     {
         float distance = Vector3.Distance(transform.position, Player.transform.position);
-        
+
+        Vector3 playerCollisionPos = new Vector3(
+            Player.transform.position.x,
+            Player.transform.position.y - (Player.transform.localScale.y / 10),
+            Player.transform.position.z
+        );
+
         if (distance < pickupRange)
         {
-            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, flightSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, playerCollisionPos, flightSpeed * Time.deltaTime);
         }
     }
 
@@ -37,10 +55,19 @@ public class Pickup : MonoBehaviour
                 {
                     stomach.FoodAmount = stomach.FoodAmount + 1;
                     print("Player has eaten food!");
+                    popSound.Play();
                     Destroy(gameObject);
                     foodSpawner.CreateFood();
-                    Vector3 newScale = Player.transform.localScale + new Vector3(grow, grow, grow);
-                    Player.transform.localScale = newScale;
+                    Vector3 playerCurrentSize = GameObject.Find("Player_Prefab").transform.localScale;
+                    Vector3 playerGrowSize = new Vector3(playerCurrentSize.x + playerGrowAmount, playerCurrentSize.y + playerGrowAmount, playerCurrentSize.z + playerGrowAmount);
+                    GameObject.Find("Player_Prefab").transform.localScale = Vector3.Lerp(playerCurrentSize, playerGrowSize, Mathf.SmoothStep(0f, 1f, playerGrowSpeed * Time.deltaTime));
+                    if (stomach.FoodAmount % 20 == 0) {
+                        vcam.m_Lens.OrthographicSize = Mathf.Lerp(
+                            vcam.m_Lens.OrthographicSize, 
+                            vcam.m_Lens.OrthographicSize + cameraGrowAmount, 
+                            cameraGrowSpeed * Time.deltaTime
+                        );
+                    }
                 }
             }
 
