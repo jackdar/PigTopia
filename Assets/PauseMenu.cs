3e01434ad3a363cdd185ba567f7b68a90457f913
Peter <132334331+bfq1427@users.noreply.github.com>
2023-10-03 17:04:11 +1300

    Created basic pause menu for game

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+
+
+}
